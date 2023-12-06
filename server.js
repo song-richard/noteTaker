9e04fd7165f71ed7d0679b2c1ec49620f20820d0
song-richard <rsong1993@gmail.com>
2023-12-06 11:39:57 -0800

    Added initial GET request path to /notes. Added middleware to handle static files

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile('./notes.html');
+})
 
 app.listen(`${PORT}`, (req, res) => {
     console.log(`Listening on PORT: ${PORT}`);
