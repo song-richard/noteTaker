ae34f8583efa2350724f06b9f14caf32ee2ce263
song-richard <rsong1993@gmail.com>
2023-12-06 13:27:35 -0800

    Added route for GET request to retrieve saved notes. In addition to parasing the json data

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'develop', 'public')));
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'develop', 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        const notes = JSON.parse(data);
+        res.json(notes);
+      }
+    });
+  });
 
 app.get('/notes', (req, res) => {
     res.sendFile('notes.html', { root: path.join(__dirname, 'develop', 'public') });
