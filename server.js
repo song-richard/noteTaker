28275f8e4f9c6f4ad9ad13fa3fa5668483dec802
song-richard <rsong1993@gmail.com>
2023-12-06 13:37:59 -0800

    Added POST request to handle note save via fs. Added express middleware for json parasing

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'develop', 'public')));
+app.use(express.json())
 
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, 'develop', 'db', 'db.json'), 'utf8', (err, data) => {
@@ -18,6 +20,40 @@ app.get('/api/notes', (req, res) => {
       }
     });
   });
+
+
+  app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'develop', 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        const notes = JSON.parse(data);
+  
+        const newNote = {
+          id: uuidv4(),
+          title: req.body.title,
+          text: req.body.text, 
+        };
+  
+        notes.push(newNote);
+  
+        fs.writeFile(
+          path.join(__dirname, 'develop', 'db', 'db.json'),
+          JSON.stringify(notes),
+          (err) => {
+            if (err) {
+              console.error(err);
+              res.status(500).json({ error: 'Internal Server Error' });
+            } else {
+              res.json(newNote);
+            }
+          }
+        );
+      }
+    });
+  });
+  
 
 app.get('/notes', (req, res) => {
     res.sendFile('notes.html', { root: path.join(__dirname, 'develop', 'public') });
