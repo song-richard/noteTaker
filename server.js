48687e967beacdb6192ee3422cee057658a3b3d7
song-richard <rsong1993@gmail.com>
2023-12-06 13:42:38 -0800

    Added notes to server.js

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+//Port used
 const PORT = process.env.PORT || 3000;
 
+//Middleware
 app.use(express.static(path.join(__dirname, 'develop', 'public')));
 app.use(express.json())
 
+//GET route and request to retrieve current notes
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, 'develop', 'db', 'db.json'), 'utf8', (err, data) => {
       if (err) {
@@ -21,7 +24,7 @@ app.get('/api/notes', (req, res) => {
     });
   });
 
-
+//POST request and route for posting new notes
   app.post('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, 'develop', 'db', 'db.json'), 'utf8', (err, data) => {
       if (err) {
@@ -29,7 +32,8 @@ app.get('/api/notes', (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
       } else {
         const notes = JSON.parse(data);
-  
+        
+        //Creates note with a unique ID via UUID, and pulls the inputted user data from the req.body, then pushes the new note to the json db
         const newNote = {
           id: uuidv4(),
           title: req.body.title,
@@ -54,16 +58,17 @@ app.get('/api/notes', (req, res) => {
     });
   });
   
-
+//GET route for /notes
 app.get('/notes', (req, res) => {
     res.sendFile('notes.html', { root: path.join(__dirname, 'develop', 'public') });
 });
 
+//GET route for '*' wildcard/all other routes
 app.get('*', (req, res) => {
     res.sendFile('index.html', { root: path.join(__dirname, 'develop', 'public') });
 });
 
+//Started server on port listed on 'PORT'
 app.listen(`${PORT}`, (req, res) => {
     console.log(`Listening on PORT: ${PORT}`);
 });
-
